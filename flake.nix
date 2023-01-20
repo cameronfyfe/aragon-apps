3a423c1de52c0f083b422f5cf1948a9062037cac
cameronfyfe <cameron.j.fyfe@gmail.com>
2023-01-19 21:58:39 -0700

    add nix shell

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-old.url = "github:NixOS/nixpkgs?rev=0e6945185b65ed6a163501d66420f8a16e49e4c3";
+  };
+
+  outputs = inputs @ { self, ... }:
+    (inputs.flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+        };
+
+        pkgs-old = import inputs.nixpkgs-old {
+          inherit system;
+        };
+
+        pkgs-old-old = import inputs.nixpkgs-old-old {
+          inherit system;
+        };
+
+        eqty.pkgs = inputs.eqty-nix.packages.${system};
+
+        commonPkgs = with pkgs; [
+          just
+        ];
+
+        nodeAndYarn = nodejs: [ nodejs (pkgs.yarn.override { inherit nodejs; }) ];
+
+        nodeAndYarn16 = nodeAndYarn pkgs.nodejs-16_x;
+        nodeAndYarn12 = nodeAndYarn pkgs-old.nodejs-12_x;
+
+      in
+      rec {
+
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = commonPkgs;
+          };
+          node16 = pkgs.mkShell {
+            buildInputs = commonPkgs ++ nodeAndYarn16;
+          };
+          node12 = pkgs.mkShell {
+            buildInputs = commonPkgs ++ nodeAndYarn12;
+          };
+        };
+
+      }));
+}
